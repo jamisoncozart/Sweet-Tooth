3cd419f105e4c44125c17968b15212f86e7bd4ea
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-27 15:08:43 -0700

    Add functionality to record user data and views for showing user data

--- SweetTooth/Models/UserStats.cs
+++ SweetTooth/Models/UserStats.cs
@@ -3,10 +3,15 @@ namespace SweetTooth.Models
   public class UserStats
   {
     public int UserStatsId { get; set; }
-    public int UserId { get; set; }
+    public string UserId { get; set; }
     public int TimesCreatedSweet { get; set; }
     public int TimesEdittedSweet { get; set; }
     public int TimesDeletedSweet { get; set; }
     public int TimesViewedSweet { get; set; }
+
+    public UserStats(string userId)
+    {
+      UserId = userId;
+    }
   }
 }
